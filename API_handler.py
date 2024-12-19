6c08352b48a8347e34fb85e0880f5ade5bad026f
Jéssica Nadalete <jessica.nadalete@lsinn.com.br>
2024-12-19 08:05:15 -0300

    First commit

---
+++ API_handler.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+from HealthInsurance import HealthInsurance
+
+#loading model
+with open('model/linear_model.pkl', 'rb') as file:
+  model = pickle.load(file)
+
+app = Flask( __name__ )
+
+@app.route( '/health_insurance/predict', methods=['POST'] )
+def HealthInsurance_predict():
+    test_json = request.get_json()
+    if test_json: # there is data
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0])
+        else:
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys()) #multiple examples
+
+        #instantiate HealthInsurance Class
+        pipeline = HealthInsurance()
+
+        #data cleaning
+        df1 = pipeline.clean_data( test_raw )
+
+        #feature enginnering
+        df2 = pipeline.data_preparation( df1 )
+
+        #prediction
+        df_response = pipeline.get_prediction( model, test_raw, df2 )
+
+        return df_response
+
+    else:
+      return Response( '{}', status=200, mimetype='application/json' )
+
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run( host='0.0.0.0', port=port)
