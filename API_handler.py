323c6c6e8911a3ba52e59dc0dc2fdbb5b34b49a9
Jéssica Nadalete <jessica.nadalete@lsinn.com.br>
2024-12-25 16:07:18 -0300

    Adusting

--- API_handler.py
+++ API_handler.py
@@ -39,4 +39,4 @@ def health_insurance_predict():
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run( host='0.0.0.0', port=port)
+    app.run( host='0.0.0.0', port=port, debug=True)
