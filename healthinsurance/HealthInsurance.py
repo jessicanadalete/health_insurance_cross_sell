730f441d7eecf4014f8ebcac6fb949ad9cc8b8fa
Jéssica Nadalete <jessica.nadalete@lsinn.com.br>
2024-12-28 18:41:47 -0300

    adjusting

--- healthinsurance/HealthInsurance.py
+++ healthinsurance/HealthInsurance.py
@@ -7,7 +7,7 @@ import pandas as pd
 class HealthInsurance( object ):
   def __init__( self ):
     self.home_path = '/opt/render/project/src/parameter/'
-    self.annual_premium_standart              = pickle.load( open( self.home_path + 'annual_premium.pkl', 'rb') )
+    self.annual_premium_standard              = pickle.load( open( self.home_path + 'annual_premium.pkl', 'rb') )
     self.age_scaler                           = pickle.load( open( self.home_path + 'age.pkl', 'rb') )
     self.vintage_scaler                       = pickle.load( open( self.home_path + 'vintage.pkl', 'rb') )
 
@@ -18,7 +18,7 @@ class HealthInsurance( object ):
                 'Policy_Sales_Channel', 'Vintage', 'Response']
 
     snakecase = lambda x: inflection.underscore(x)
-    new_columns = list(map(snakecase, old_columns))
+    new_columns = list(map(self.snakecase, old_columns))
 
     df1.columns = new_columns
 
@@ -27,11 +27,12 @@ class HealthInsurance( object ):
   def data_preparation (self, df5):
 
     #standardization
-    df5['annual_premium'] = self.stand_annual_premium.transform(df5[['annual_premium']].values)
+    df5['annual_premium'] = self.stand_annual_premium.fit_transform(df5[['annual_premium']].values)
 
     #recasling
-    df5['age'] = self.age_scaler.transform(df5[['age']].values)
-    df5['vintage'] = self.vintage_scaler.transform(df5[['vintage']].values)
+    df5['age'] = self.age_scaler.fit_transform(df5[['age']].values)
+    
+    df5['vintage'] = self.vintage_scaler.fit_transform(df5[['vintage']].values)
 
     #vehicle_damage (Label encoding)
     df5['vehicle_damage'] = df5['vehicle_damage'].apply(lambda x: 1 if x == "Yes" else 0)
